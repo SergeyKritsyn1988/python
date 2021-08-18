2db01318c399ef5867df43ddb82bdd7fda622c16
SergeyKritsyn1988 <88883258+SergeyKritsyn1988@users.noreply.github.com>
2021-08-18 15:21:07 +0300

    Delete Lesson_1 directory

--- Lesson_1/1.3.py
+++
@@ -1,3 +0,0 @@
-number=int(input("please enter a number "))
-
-print(number + number*11 + number*111)
--- Lesson_1/1.4.py
+++
@@ -1,8 +0,0 @@
-umber=int(input("please enter a number "))
-m = number%10
-number = number//10
-while number > 0:
-    if number%10 > m:
-        m = number%10
-    number = number//10
-print(m)
